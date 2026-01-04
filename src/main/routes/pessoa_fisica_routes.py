f4e57263f81f8e5f5d9d6d78120a0c453173f85e
Anderson Fernandes <anderson.fernandes96@outlook.com>
2026-01-04 11:22:53 -0300

    feat: Add error handle in routes

--- src/main/routes/pessoa_fisica_routes.py
+++ src/main/routes/pessoa_fisica_routes.py
@@ -4,47 +4,65 @@ from src.views.http_types.http_request import HttpRequest
 
 from src.main.composer.pessoa_fisica_composer import pessoa_fisica_composer
 
+from src.errors.error_handle import handle_errors
+
 pessoa_fisica_routes_bp = Blueprint("pessoa_fisica_routes", __name__)
 
 @pessoa_fisica_routes_bp.route("/pessoa-fisica", methods=["POST"])
 def create_pessoa_fisica():
-    body = request.json or {}
-    # Define a ação para o método handle processar corretamente
-    body["action"] = "criar"
-    http_request = HttpRequest(body=body)
-    view = pessoa_fisica_composer()
+    try:
+        body = request.json or {}
+        # Define a ação para o método handle processar corretamente
+        body["action"] = "criar"
+        http_request = HttpRequest(body=body)
+        view = pessoa_fisica_composer()
 
-    http_response = view.handle(http_request)
-    return jsonify(http_response.body), http_response.status_code
+        http_response = view.handle(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @pessoa_fisica_routes_bp.route("/pessoa-fisica/<int:person_id>", methods=["GET"])
 def get_pessoa_fisica(person_id):
-    http_request = HttpRequest(
-        body={ "action": "buscar" },
-        param={ "id": person_id }
-    )
-    view = pessoa_fisica_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(
+            body={ "action": "buscar" },
+            param={ "id": person_id }
+        )
+        view = pessoa_fisica_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @pessoa_fisica_routes_bp.route("/pessoa-fisica/<int:person_id>/sacar", methods=["POST"])
 def sacar_pessoa_fisica(person_id):
-    body = request.json or {}
-    body["action"] = "sacar"
-    http_request = HttpRequest(body=body, param={"id": person_id})
-    view = pessoa_fisica_composer()
+    try:
+        body = request.json or {}
+        body["action"] = "sacar"
+        http_request = HttpRequest(body=body, param={"id": person_id})
+        view = pessoa_fisica_composer()
 
-    http_response = view.handle(http_request)
-    return jsonify(http_response.body), http_response.status_code
+        http_response = view.handle(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @pessoa_fisica_routes_bp.route("/pessoa-fisica/<int:person_id>/extrato", methods=["GET"])
 def extrato_pessoa_fisica(person_id):
-    http_request = HttpRequest(
-        body={ "action": "extrato" },
-        param={ "id": person_id }
-    )
-    view = pessoa_fisica_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(
+            body={ "action": "extrato" },
+            param={ "id": person_id }
+        )
+        view = pessoa_fisica_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
